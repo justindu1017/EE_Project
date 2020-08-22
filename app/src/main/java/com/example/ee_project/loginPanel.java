113030dda29c3bfbd173f9eecabc7b189efc03d9
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2020-08-22 20:12:03 +0800

    temp upload fixing network

--- app/src/main/java/com/example/ee_project/loginPanel.java
+++ app/src/main/java/com/example/ee_project/loginPanel.java
@@ -1,77 +1,3 @@
-//package com.example.ee_project;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.os.AsyncTask;
-//import android.os.Bundle;
-//
-//import java.io.BufferedInputStream;
-//import java.io.BufferedReader;
-//import java.io.DataOutputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//import java.net.URLEncoder;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        Load load = new Load();
-//        load.execute();
-//    }
-//    class Load extends AsyncTask<String , String, String> {
-//
-//        @Override
-//        protected String doInBackground(String... strings) {
-//            try {
-//                String userName = "TestUser";
-//
-//                URL url = new URL("http://10.0.2.2/index.php");
-//                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//                connection.setRequestMethod("POST");
-//                connection.setDoInput(true);
-//                connection.setDoOutput(true);
-//
-//                DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
-//                StringBuilder stringBuilder = new StringBuilder();
-//
-//
-//                stringBuilder.append("userName=").append(URLEncoder.encode("userName","utf-8"));
-//                dataOutputStream.writeBytes(stringBuilder.toString());
-//                dataOutputStream.flush();
-//                dataOutputStream.close();
-//
-//
-//
-//                System.out.println("code is "+connection.getResponseCode());
-//                System.out.println("OKKKKK");
-//
-//
-//
-//                InputStream is = new BufferedInputStream(connection.getInputStream());
-//                InputStreamReader inputStreamReader = new InputStreamReader(is);
-//                BufferedReader reader = new BufferedReader(inputStreamReader);
-//                StringBuffer sb = new StringBuffer();
-//                String str;
-//                while((str = reader.readLine())!= null){
-//                    sb.append(str);
-//                }
-//                System.out.println("res is       "+sb.toString());
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            return  null;
-//        }
-//    }
-//}
-//
-//
-
 package com.example.ee_project;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -94,7 +20,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class loginPanel extends AppCompatActivity {
     EditText loginAccount, loginPassword;
     Button loginButton;
 
