7cd2724bc70c2bb12b87dde32d5d6909e6920639
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2020-09-18 03:28:50 +0800

    building recyclerview

---
+++ app/src/main/java/com/example/ee_project/ArticalListPage.java
@@ -0,0 +1,14 @@
+package com.example.ee_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArticalListPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artical_list_page);
+    }
+}
